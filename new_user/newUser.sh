8652180f62389ee446b246b38f02f929b2fc821a
Michael Flood <156374637+michaelflood1@users.noreply.github.com>
2024-11-10 23:56:38 -0800

    Update newUser.sh

--- new_user/newUser.sh
+++ new_user/newUser.sh
@@ -16,36 +16,36 @@ if [[ "$EUID" -ne 0 ]];
 	exit 1
 fi
 
-# 1/0 Boolean flag for my optional arguments
+
 #parse options with getopts and proceed with a different method for each
-username=""
+username="" #used to store -u arg soon
 #parse options with getopts and proceed with a different method for each
-while getopts ":u:g:s:h" opt;
+while getopts ":u:g:s:h" opt; # allows us to make a case statement and use the input args from user
 do
 	
 	case $opt in
 		u)
-			if [[ -z ${OPTARG} ]] ||  [[ -d ~/${OPTARG} ]] ;
+			if [[ -z ${OPTARG} ]] ||  [[ -d ~/${OPTARG} ]] ; # if home dir exists the user is already create, if the optarg doesnt exist we cannot follow the script
 			then
 				echo "this user is already created or 
 				additional argument require see
 				-u[username]"
 				exit 1
 			else
-				useradd -d ~/${OPTARG}
+				useradd -d ~/${OPTARG} "${OPTARG}  #adds user then calls to create a password
 				echo " add a password for the user"
 				passwd ${OPTARG}
-				username=${OPTARG}
+				username=${OPTARG} # stores username for later
 			fi
 			;;
 		g)
-			if [[ -z ${OPTARG} ]] || [[ -z ${username} ]];
+			if [[ -z ${OPTARG} ]] || [[ -z ${username} ]]; # standard error check to stop typos
 			then
 				echo " additional argument required see
 				-g["group"]"
 				exit 1
 			else
-				usermod -aG ${OPTARG} ${username}
+				usermod -aG ${OPTARG} ${username} #adds group to specified user
 			fi
 			;;
 		s)
@@ -54,11 +54,11 @@ do
 				echo "additional argument required see
 				-s[""]"
 			else
-				usermod -s /etc/shells/${OPTARG} ${username}
+				usermod -s /etc/shells/${OPTARG} ${username} # adds a shell/enviorenment variable to the user
 			fi
 			;;
 		
-		h)
+		h) # provides detailed info to the user
 			echo "this script requires root or sudo
 			privelages.options -u -g -s 
 			((username, groups, shell)) require
@@ -67,7 +67,7 @@ do
 			exit 1
                         ;;
 
-		*)
+		*) # submits correct syntax for the user that sent a wrong input
 			echo "useage:
                         sudo mandatory -u[username] 
 			-g[grouptoadduserto] -s[usersprefferedshell] -h"
